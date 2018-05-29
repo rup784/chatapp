c9930191db1a647d2d1a44d28b384b1c67b5e63e
Bhawasar, Rupesh <rbhawasar@levi.com>
2018-05-29 10:51:27 +0530

    updates made

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-(function () {
-
   var app = require('express')();
   var http = require('http').Server(app);
   var io = require('socket.io')(http);
 
 
   app.get('/', function (req, res) {
-    process.chdir("../");
-    res.sendfile(process.cwd() + '/chat.html');
+    // process.chdir("../");
+    // res.sendfile(process.cwd() + '/chat.html');
+    res.sendfile('chat.html');
   });
 
   io.on('connection', function (socket) {
@@ -22,5 +21,3 @@
   http.listen(3000, function () {
     console.log('listening on *:3000');
   });
-
-})();
